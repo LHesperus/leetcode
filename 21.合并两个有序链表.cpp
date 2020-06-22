28d009991a3311dcdde4dee1049e8c429c2d3576
LHesperus <2639350497@qq.com>
2020-06-22 22:31:49 +0800

    Update 21.合并两个有序链表.cpp

--- 21.合并两个有序链表.cpp
+++ 21.合并两个有序链表.cpp
@@ -18,48 +18,26 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode *ans(0);
-        ListNode *ans2(ans);
-        bool flag1=false,flag2=false;
-        while(l1!=nullptr||l2!=nullptr)
-        {
-            if(l1==nullptr&&l2!=nullptr)
-            {
-                ans->val=l2->val;
-                if(l2->next) ans->next=new ListNode(0);
-                else return ans2;
-                
-                l2=l2->next;
-            }else if(l1!=nullptr&&l2==nullptr)
-            {
-                ans->val=l1->val;
-                if(l1->next) ans->next=new ListNode(0);
-                else return ans2;
-                
-                l1=l1->next;
-            }else if(l1!=nullptr&&l2!=nullptr)
-            {
-                if(l1->val>l2->val)
-                {
-                    ans->val=l2->val;
-                    if(l2->next==nullptr&&l1==nullptr)return ans2;
-                    else ans->next=new ListNode(0);  
-                    if(l2->next) l2=l2->next;
-                    else l2=nullptr; 
-                }else
-                {
-                    ans->val=l1->val;
-                    
-                    if(l1->next==nullptr&&l2==nullptr) return ans2;
-                    else  ans->next=new ListNode(0);
-                    if(l1->next) l1=l1->next;
-                    else l1=nullptr;
-                }
-            }
-           
-
-        }
-        return ans2;
+       
+       ListNode* ans = new ListNode(-1);
+       ListNode *anstmp=ans;
+       //ListNode *ans(anstmp);
+       while(l1&&l2)
+       {
+           if(l1->val>l2->val)
+           {
+               anstmp->next=l2;
+               l2=l2->next;
+           }else
+           {
+               anstmp->next=l1;
+               l1=l1->next;
+           }
+           anstmp=anstmp->next;
+       }
+       anstmp->next = l1 == nullptr ? l2 : l1;
+       //l1==nullptr? anstmp->next=l2:anstmp->next=l1;
+       return ans->next;
     }
 };
 // @lc code=end
